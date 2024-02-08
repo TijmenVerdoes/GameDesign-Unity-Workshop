e3acba217f5cc3cf9bb305510c3096d1b0f664d3
Tijmen <tijmenverdoes10@gmail.com>
2024-02-08 10:55:35 +0100

    Add camera script

---
+++ Assets/Scripts/Camera Script.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float sensitivity = 10f;
+    void Update ()
+    {
+        var c = Camera.main.transform;
+        c.Rotate(0, Input.GetAxis("Mouse X")* sensitivity, 0);
+        c.Rotate(-Input.GetAxis("Mouse Y")* sensitivity, 0, 0);
+        c.Rotate(0, 0, -Input.GetAxis("QandE")*90 * Time.deltaTime);
+        if (Input.GetMouseButtonDown(0))
+            Cursor.lockState = CursorLockMode.Locked;
+    }
+}
