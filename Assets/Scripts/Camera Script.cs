373990a76b74fa82de7ac3ddc40effc6c6adcbe0
Tijmen <tijmenverdoes10@gmail.com>
2024-02-08 11:02:47 +0100

    Temp Camera Script

--- Assets/Scripts/Camera Script.cs
+++ Assets/Scripts/Camera Script.cs
@@ -7,11 +7,6 @@ public class CameraScript : MonoBehaviour
     public float sensitivity = 10f;
     void Update ()
     {
-        var c = Camera.main.transform;
-        c.Rotate(0, Input.GetAxis("Mouse X")* sensitivity, 0);
-        c.Rotate(-Input.GetAxis("Mouse Y")* sensitivity, 0, 0);
-        c.Rotate(0, 0, -Input.GetAxis("QandE")*90 * Time.deltaTime);
-        if (Input.GetMouseButtonDown(0))
-            Cursor.lockState = CursorLockMode.Locked;
+
     }
 }
